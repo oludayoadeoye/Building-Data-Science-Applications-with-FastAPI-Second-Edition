96c1cc5fb1a88400916a3a61961d433ad5c8bc89
oludayoadeoye <79817345+oludayoadeoye@users.noreply.github.com>
2025-01-28 16:42:39 +0000

    it worked

--- chapter06/mongodb_relationship/database.py
+++ chapter06/mongodb_relationship/database.py
@@ -1,9 +1,8 @@
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 
-# Connection to the whole server
-motor_client = AsyncIOMotorClient("mongodb://localhost:27017")
-# Single database instance
-database = motor_client["chapter06_mongo"]
+uri = "mongodb://admin:admin@localhost:27017/?authSource=admin"
+client = AsyncIOMotorClient(uri)
+database = client["chapter06_mongo"]
 
 
 def get_database() -> AsyncIOMotorDatabase:
